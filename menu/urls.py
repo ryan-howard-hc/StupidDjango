fb89465e0cea8de58ef91d8774c214a0dd82c0ca
root <ryan.howard1993@gmail.com>
2023-07-28 01:28:58 +0000

    pre json addition in case it breaks

--- menu/urls.py
+++ menu/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.default_menu, name='default_menu'),
     path('get_table/', views.get_table, name='get_table'),
     path('get_menu/', views.get_menu, name='get_menu'),
+    path('add_menu_items/', views.add_menu_items_to_database, name='add_menu_items'),
 ]
